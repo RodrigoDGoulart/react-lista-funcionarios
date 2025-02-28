2728e89de6732049debe6746a250cb16f83a4bea
RodrigoDGoulart <rodinizgoulart@gmail.com>
2025-02-27 22:57:25 -0300

    feat: search input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
+import { MagnifyingGlass } from "phosphor-react";
 import Header from "./components/Header";
+import Input from "./components/Input";
 import Table from "./components/Table";
 
 function App() {
   return (
     <>
       <Header />
-      <Table content={[
-      { foto: "", nome: "João Silva", cargo: "Desenvolvedor", dataAdmissao: new Date(), telefone: "123" },
-      { foto: "", nome: "Maria Souza", cargo: "Designer", dataAdmissao: new Date(), telefone: "123" },
-      { foto: "", nome: "Carlos Pereira", cargo: "Gerente de Projetos", dataAdmissao: new Date(), telefone: "123" }
-      ]} />
+      <Table
+        content={[
+          {
+            foto: "",
+            nome: "João Silva",
+            cargo: "Desenvolvedor",
+            dataAdmissao: new Date(),
+            telefone: "123",
+          },
+          {
+            foto: "",
+            nome: "Maria Souza",
+            cargo: "Designer",
+            dataAdmissao: new Date(),
+            telefone: "123",
+          },
+          {
+            foto: "",
+            nome: "Carlos Pereira",
+            cargo: "Gerente de Projetos",
+            dataAdmissao: new Date(),
+            telefone: "123",
+          },
+        ]}
+      />
+      <Input placeholder="teste" icon={<MagnifyingGlass weight="bold" />} />
     </>
   );
 }
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import styles from "./style.module.scss";
+
+interface Props
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  icon?: React.ReactElement<{ className?: string, size: number }>;
+}
+
+export default function Input({ icon, ...inputProps }: Props) {
+  return (
+    <span className={styles.input}>
+      <input {...inputProps} />
+      {icon && React.cloneElement(icon, {className: styles.icon, size: 16})}
+    </span>
+  );
+}
